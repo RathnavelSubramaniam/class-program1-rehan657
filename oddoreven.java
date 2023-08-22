b49c0051fd641b54e19f889e491df1b3133b11da
rehan657 <142786788+rehan657@users.noreply.github.com>
2023-08-22 09:28:51 +0000

    completed odd or even

---
+++ oddoreven.java
@@ -0,0 +1,11 @@
+class oddoreven{
+    public static void main(String args[]){
+int num =3;
+if(num%2==0){
+    System.out.println("the given number is ODD");
+}
+else{
+    System.out.println("the given number is EVEN");
+  }
+ }
+}
